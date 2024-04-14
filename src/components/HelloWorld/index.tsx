c94df1df9406bb5f96c4bdc45dfef9487681fe6a
CallumBoase <callum.boase@gmail.com>
2024-04-14 10:55:43 +1000

    Clean before compile

--- src/components/HelloWorld/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const HelloWorld = () => {
-  return <div>Hello, World!</div>;
-}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export { SupabaseProvider } from "./components/SupabaseProvider";
 export { SupabaseProviderMeta } from "./components/SupabaseProvider/registerComponentMeta";
-export { HelloWorld } from "./components/HelloWorld";
